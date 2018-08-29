a509a1b90975820bb86831b84fcab344d809cd28
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-29 12:54:49 -0500

    Styling and content updates

--- src/Introduction.tsx
+++ src/Introduction.tsx
@@ -17,18 +17,26 @@ class Introduction extends React.Component<Props> {
         <div className="col-4 bg-dark">
           <div className="row mt-4">
             <div className="col-12 mt-1">
-              <span className="intro-header">
+              <header className="intro-header">
                 Welcome!
-              </span>
-              <p><span className="intro-paragraph">
+              </header>
+              <header className="intro-sub-header py-3 my-3 border-bottom border-top">
+              My name is Ed Pantzar and this site demonstrates my professional abilities!
+              </header>
+              <span className="intro-paragraph">
+              <p>
               Before learning to program, I obtained a degree in Integrative Physiology, 
               worked in technical sales, and excelled in digital marketing. The transition 
               to full-time development has been extremely enjoyable because it has allowed 
               me to be analytical and creative while taking projects from the design stage 
               through to functionality. Now, I'm looking forward to working with companies 
               that build user-centric products and positively impact the world around me.
-              </span>
               </p>
+              </span>
+              <button type="button" className="btn btn-outline-success mr-1 mt-1">Web Development</button>
+              <button type="button" className="btn btn-outline-success mr-1 mt-1">Design</button>
+              <button type="button" className="btn btn-outline-success mr-1 mt-1">Digital Marketing</button>
+              <button type="button" className="btn btn-outline-success mr-1 mt-1">Physiology</button>
             </div>
           </div>
         </div>
