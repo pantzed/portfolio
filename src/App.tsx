622e6c9a9a134a0e1e95a24024ff33cd94f093bc
edpantzar@gmail.com <edpantzar@gmail.com>
2018-08-28 20:14:43 -0500

    Added nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import * as React from 'react';
+import { Navbar, Button, Alignment } from "@blueprintjs/core";
+import '../node_modules/normalize.css/normalize.css';
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css';
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css';
 import './App.css';
 
-import logo from './logo.svg';
+export interface Props {
+  theme?: string;
+}
 
-class App extends React.Component {
+interface State {
+  currentTheme: string;
+}
+
+class App extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { 
+      currentTheme: props.theme || "light",
+    }
+  }
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Navbar className="bp3-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Ed Pantzar</Navbar.Heading>
+          <Navbar.Divider />
+        </Navbar.Group>
+        <Navbar.Group align={Alignment.RIGHT}>
+            <Button className="bp3-minimal" icon="person" text="Intoduction" />
+            <Button className="bp3-minimal" icon="application" text="Projects" />
+            <Button className="bp3-minimal" icon="clipboard" text="Experience" />
+            <Button className="bp3-minimal" icon="mobile-phone" text="Contact" />
+        </Navbar.Group>
+      </Navbar>
     );
   }
 }
