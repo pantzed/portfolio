85b936aa1f58319170999f97717e5e56968a9ece
edpantzar@gmail.com <edpantzar@gmail.com>
2018-09-03 19:43:11 -0500

    Added projects to db and map jsx with api call

--- client/src/Projects.tsx
+++ client/src/Projects.tsx
@@ -19,9 +19,8 @@ export class Projects extends React.Component<Props, State>{
 
   mapProjects(){
     const projectsJSX = this.state.projects.map((project: any, index: number) => {
-      console.log(project);
       return (
-        <div className="row" key={index}>
+        <div className="row mt-5" key={index}>
           <div className="col-12">
             <div className="card shadow-sm">
               <img className="card-img-top" src={ project.img } alt="Card image cap" />
@@ -62,52 +61,9 @@ export class Projects extends React.Component<Props, State>{
           <span className="intro-header">Projects</span>
         </div>
       </div>
-      <div className="row d-flex justify-content-center mt-3 mb-5">
+      <div className="row d-flex justify-content-center mb-5">
         <div className="col-11">
         { this.mapProjects() }
-          <div className="row mt-5">
-            <div className="col-12">
-              <div className="card shadow-sm">
-                <img className="card-img-top" src="https://images.pexels.com/photos/696218/pexels-photo-696218.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="Card image cap" />
-                <div className="card-body">
-                <h5 className="card-title"><span className="font-weight-bold">New Here</span></h5>
-                  <p className="card-text">  
-                    A social site that helps people find activities when they are new to a place.
-                  </p>
-                  <a href="https://github.com/pantzed" className="btn btn-2">Open Project</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col-12">
-              <div className="card shadow-sm">
-                <img className="card-img-top" src="https://images.pexels.com/photos/590020/pexels-photo-590020.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="Card image cap" />
-                <div className="card-body">
-                <h5 className="card-title"><span className="font-weight-bold">Energy Live</span></h5>
-                  <p className="card-text">  
-                    A revision of the original "energy-live" using node/express to serve a webpage 
-                    showing electrical energy data.
-                  </p>
-                  <a href="https://github.com/pantzed" className="btn btn-2">Open Project</a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col-12">
-              <div className="card shadow-sm">
-                <img className="card-img-top" src="https://images.pexels.com/photos/1044989/pexels-photo-1044989.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350" alt="Card image cap" />
-                <div className="card-body">
-                <h5 className="card-title"><span className="font-weight-bold">Pixel Art Maker</span></h5>
-                  <p className="card-text">  
-                    An artistic outlet for creating pixelated artwork.
-                  </p>
-                  <a href="https://github.com/pantzed" className="btn btn-2">Open Project</a>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
--- seeds/0000_projects.js
+++ seeds/0000_projects.js
@@ -11,7 +11,28 @@ exports.seed = function(knex, Promise) {
           img: 'https://images.pexels.com/photos/542556/pexels-photo-542556.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
           url: 'https://stroke-with-friends.herokuapp.com/',
           git: 'https://github.com/pantzed/stroke-with-friends'
-        }
+        },
+        {
+          title: 'New Here', 
+          description: 'A social site that helps people find activities when they are new to a place.',
+          img: 'https://images.pexels.com/photos/696218/pexels-photo-696218.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+          url: 'https://new-here.herokuapp.com/',
+          git: 'https://github.com/pantzed/new-here'
+        },
+        {
+          title: 'Energy Live', 
+          description: 'A revision of the original "energy-live" using node/express to serve a webpage showing electrical energy data.',
+          img: 'https://images.pexels.com/photos/45072/pexels-photo-45072.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+          url: 'http://eds-pixel-art-maker.surge.sh/',
+          git: 'https://github.com/pantzed/energy-live2'
+        },
+        {
+          title: 'Pixel Art Maker', 
+          description: 'An artistic outlet for creating pixelated artwork.',
+          img: 'https://images.pexels.com/photos/1044989/pexels-photo-1044989.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=350',
+          url: 'https://energy-live.herokuapp.com/',
+          git: 'https://github.com/pantzed/pixel-art-maker'
+        },
       ]);
     });
 };
